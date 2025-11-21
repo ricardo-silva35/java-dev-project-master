4e9bcdede70ed100fdb057f755b3f4cd1d5f923e
ricardo-silva35 <ricardosilva.santosal200@gmail.com>
2025-11-21 01:08:51 -0300

    New Class

---
+++ javaDevProjectMaster/src/javaDevProjectMaster/WorkingGit.java
@@ -0,0 +1,24 @@
+package javaDevProjectMaster;
+
+public class WorkingGit {
+	
+		public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+		private String name;
+		
+		private String lastName;
+}
